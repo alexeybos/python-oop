9a31cc98a50a11859c411e2e04943deca854a251
Alexey.Bosalyga <alexey.bosalyga@nexign.com>
2025-11-09 22:01:58 +0400

    lesson03

---
+++ lesson03/task03.py
@@ -0,0 +1,103 @@
+class Territory:
+    def __init__(self, height, width, owner):
+        self.height = height # meters
+        self.width = width # meters
+        self.owner = owner
+
+    def is_occupied(self):
+        return self.owner is not None and self.owner != ''
+
+class Garden(Territory):
+    def __init__(self, height, width, owner, tree_count):
+        super().__init__(height, width, owner)
+        self.tree_count = tree_count
+
+    def plant_tree(self, count):
+        self.tree_count += count
+
+    def cut_down_tree(self, count):
+        self.tree_count -= count
+
+class Field(Territory):
+    def __init__(self, height, width, owner):
+        super().__init__(height, width, owner)
+        self.ripening_date = None
+        self.seed_culture = ''
+        self.productivity = 0 # урожайность (кг с м**2)
+        self.is_planted = False
+
+    def seed(self, seed_culture, productivity, ripening_date):
+        self.seed_culture = seed_culture
+        self.productivity = productivity  # урожайность (кг с м**2)
+        self.ripening_date = ripening_date
+        self.is_planted = True
+
+    def harvest(self, harvest_date):
+        if self.is_planted and harvest_date >= self.ripening_date:
+            return self.width * self.height * self.productivity
+        return 0
+
+class Plant:
+    def __init__(self, name, plant_type, growth_per_day):
+        self.name = name
+        self.plant_type = plant_type
+        self.growth_per_day = growth_per_day  # см в день
+        self.height = 0
+        self.water_level = 50
+
+    def water(self, amount):
+        self.water_level = min(100, self.water_level + amount)
+        print(f"Полили {self.name}. Уровень воды: {self.water_level}%")
+
+    def grow(self):
+        if self.water_level > 30:
+            self.height += self.growth_per_day
+            print(f"{self.name} вырос на {self.growth_per_day}. Теперь {self.height} см")
+        else:
+            print(f"{self.name} нуждается в поливе!")
+
+    def get_info(self):
+        return f"{self.name} ({self.plant_type}), высота: {self.height} см"
+
+class Tree(Plant):
+    def __init__(self, name, plant_type, growth_per_day, circumference_per_day):
+        super().__init__(name, plant_type, growth_per_day)
+        self.circumference_per_day = circumference_per_day
+        self.circumference = 0
+        self.crown_diameter = 0
+
+    def grow(self):
+        if self.water_level > 30:
+            self.height += self.growth_per_day
+            self.circumference += self.circumference_per_day
+            self.crown_diameter += self.circumference_per_day * 2.5
+            print(f"{self.name} выросло. Теперь {self.height} см и в обхвате {self.circumference} см")
+        else:
+            print(f"{self.name} нуждается в поливе!")
+
+    def provide_shade(self, area_size):
+        shade_coverage = min(100, (self.crown_diameter / area_size) * 100)
+        print(f"{self.name} создает тень на {shade_coverage:.1f}% площади")
+        return shade_coverage
+
+class Potato(Plant):
+    def __init__(self, name, plant_type, growth_per_day, tubers_per_height):
+        super().__init__(name, plant_type, growth_per_day)
+        self.tubers_per_height = tubers_per_height
+        self.pest_count = 0
+        self.is_exterminated = False
+
+    def get_tubers_count(self):
+        return self.tubers_per_height * self.height / 10
+
+    def check_pests(self):
+        if self.is_exterminated:
+            self.is_exterminated = False
+        else:
+            self.pest_count = 2 * self.height / 10
+        
+        return self.pest_count
+
+    def extermination(self):
+        self.pest_count = 0
+        self.is_exterminated = True
